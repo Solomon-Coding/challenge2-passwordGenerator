6602c8a009f87b2924bb282d903ec288309de6c9
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2022-12-11 18:20:31 -0600

    Version 1: fully working code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,16 @@ const alphUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const num = "0123456789";
 const char = "\u0021\u0022\u0023\u0024\u0025\u0026\u0027\u0028\u0029\u002A\u002B\u002C\u002D\u002E\u002F\u003A\u003B\u003C\u003D\u003E\u003F\u0040\u005B\u005C\u005D\u005E\u005F\u0060\u007B\u007C\u007D\u007E";
 const options = [alphLower,alphUpper,num,char];
-// console.log(options[0])
-// console.log(options[1])
-// console.log(options[2])
-// console.log(options[3])
 
 // Write password to the #password input
 function writePassword() {
-  var length = 10//Number(window.prompt("Please select a length for the password between 8 and 128", ));
+  var length = Number(window.prompt("Please select a length for the password between 8 and 128", ));
   passwordLength(length);
   var var_list = passwordAttributes();
   var password = generatePassword(length,var_list);
-  // let password = pass.join("")
-  //  = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  // console.log(password.join(''))
 }
 
 // Add event listener to generate button
@@ -42,13 +35,13 @@ function passwordLength(length) {
 
 function passwordAttributes() {
   window.alert("Should the password include the following? Please type Y/N")
-  var lc = "Y"//window.prompt("Lowercase? (Y/N)",);
+  var lc = window.prompt("Lowercase? (Y/N)",);
   var lc_val = logic(lc);
-  var uc = "Y"//window.prompt("Uppercase? (Y/N)",);
+  var uc = window.prompt("Uppercase? (Y/N)",);
   var uc_val = logic(uc);
-  var n = "Y"//window.prompt("Numeric? (Y/N)",);
+  var n = window.prompt("Numeric? (Y/N)",);
   var n_val = logic(n);
-  var sc = "Y"//window.prompt("Special Characters? (Y/N)",);
+  var sc = window.prompt("Special Characters? (Y/N)",);
   var sc_val = logic(sc);
   if (lc=="N"&&uc=="N"&&n=="N"&&sc=="N"){
     passwordAttributes();
@@ -73,10 +66,8 @@ function logic(num) {
 
 function generatePassword(length,vars) {
   var includedVariables = vars[0]+vars[1]+vars[2]+vars[3];
-  // console.log(includedVariables);
   var divLength = Math.floor(length/includedVariables);
   var remainder = length%includedVariables;
-  // console.log(remainder);
   var rand = [];
   var rand2=[];
   var combinedString = "";
@@ -85,28 +76,19 @@ function generatePassword(length,vars) {
     if ((Number(vars[i]))==1){
       rand[ii] = randomize(options[i],divLength+remainder);
       rand2[ii] = rand[ii].join();
-      console.log(ii)
-      // combinedString = combinedString+rand[ii];
       if (ii!=0){
         combinedString = combinedString+","+rand[ii];
       }else{
       combinedString = combinedString+rand[ii];
-      console.log(combinedString)
       }
-      // console.log(randomize(rand[2],divLength))
-      // console.log(rand2[2].length)
       remainder = 0;
       ii = ii+1;
     } else {
       ii=ii;
     }
   }
-  
-  console.log(String(combinedString));
-  var randString = shuffle(String(combinedString));
-  // var randString = randomize(combinedString,combinedString.length);
-  console.log(randString);
-  return randString;
+  var randNum = shuffle(combinedString,length);
+  return randNum;
   
 }
 
@@ -114,16 +96,28 @@ function randomize(array,terms) {
     var randArray = [];
     for (var j=0;j<terms;j++)
     randArray[j] = array[Math.floor(Math.random()*array.length)];
-    console.log(randArray)
     return randArray;
 }
 
-function shuffle(array) {
-  for (var k = array.length - 1; k > 0; k--) {
-    var kk = Math.floor(Math.random() * (k + 1));
-    var temp = array[k];
-    array[k] = array[kk];
-    array[kk] = temp;
+function shuffle(vars,arrayLength) {
+  var kk = [];
+  for (var k=0; k < arrayLength; k++) {
+    kk[k] = k;
   }
-  return array;
+  var array = kk
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+  var randString = [];
+  m = 0;
+  var randString2 = [];
+  for (var n=0;n<(2*arrayLength)-1;n+=2){
+    randString[array[m]] = vars[n];
+    m = m+1;
+  }
+  randString2 = randString.join('') 
+   return randString2;
 }
